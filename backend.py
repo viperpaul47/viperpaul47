5825f441258e4e2afa2f61ed23c65ce1df122264
viperpaul47 <viperpaul47@gmail.com>
2026-02-14 20:33:46 +0530

    first commit

---
+++ backend.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Today's "Fake" Data (We will replace this with an API tomorrow!)
+    current_weather = "Chilly and Raining"
+    my_suggestion = "A raincoat and waterproof boots"
+    
+    return render_template('frontend.html', weather_status=current_weather, outfit=my_suggestion)
+
+if __name__ == '__main__':
+    app.run(debug=True)
