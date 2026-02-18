b2b650aef10335ce8c97f632ae9acb9524c22847
viperpaul47 <viperpaul47@gmail.com>
2026-02-18 20:41:24 +0530

    staging check

--- backend.py
+++ backend.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Today's "Fake" Data (We will replace this with an API tmr!)
     current_weather = "Chilly and Raining"
     my_suggestion = "A raincoat and waterproof boots"
     
